4e72c9e60310bea7daf82fa2a18d1b87f9d01ec7
gilmarbeta <gilmarbeta@hotmail.com>
2023-03-27 15:32:18 -0400

    Calculator final version.
    Updated final version of the Calculator 2023.

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
+<<<<<<< HEAD
 const inputAllRegex = /([^0-9=+\.\*/-])/;
 const inputSignsRegex = /([=+\.\*/-])/;
 let numberArr = [];
 
 const inputField = document.getElementById("inputField");
 
+=======
+//Variable declarations. And regexes.
+
+const inputSignsRegex = /([-+\*\/])/;
+const fullOperationRegex= /^(\d+(\.\d+)?[-+\/*]\d+(\.\d+)?)/;
+let firstValue=0;
+let secondValue=0;
+let operation = "";
+let signpos;
+
+const inputField = document.getElementById("inputField");
+
+//To restrict the input field to only one operation and grab the operation Symbol.
+
+inputField.addEventListener("beforeinput", (event)=>{
+  let beforesign = event.data;
+  console.log("Beforeinput sign: " + beforesign);
+  if(fullOperationRegex.test(inputField.value) && inputSignsRegex.test(beforesign)){
+    event.preventDefault();
+  } else if(inputSignsRegex.test(beforesign)){
+    operation = beforesign;
+    signpos = inputField.value.indexOf(operation);
+  }
+});
+
+>>>>>>> acb630b (Calculator final version.)
 //Function to get the input
 
 function getInput(event) {
   let number = event.key;
+<<<<<<< HEAD
   numberArr.push(number);
   if (/^\d/.test(numberArr[0])) {
     console.log(numberArr.length);
@@ -34,11 +62,39 @@ function getInput(event) {
     );
     inputField.value = result;
   }
+=======
+
+  if(fullOperationRegex.test(inputField.value)&& number==="Enter"){
+
+    firstValue = parseFloat(inputField.value.slice(0,signpos));
+    secondValue = parseFloat(inputField.value.slice(signpos+1));
+    let result= operate(operation, firstValue, secondValue);
+      inputField.value = result;
+      console.log("Value1: " + firstValue);
+      console.log("Value2: " + secondValue);
+      console.log("Sign: " + operation);
+      console.log(" Result: " + result);	
+  }
+
+  //To erase the entire input field.
+
+  if(number==="Escape"){
+    inputField.value = "";
+    operation = "";
+    firstValue=0;
+    secondValue=0;
+  }
+  
+>>>>>>> acb630b (Calculator final version.)
 }
 
 inputField.addEventListener("keyup", getInput);
 
+<<<<<<< HEAD
 //Function to do the operation goes here
+=======
+//Function to do the operation.
+>>>>>>> acb630b (Calculator final version.)
 
 function operate(sign, num1, num2) {
   if (sign === "+") {
@@ -70,6 +126,7 @@ const divide = function (num1, num2) {
   return num1 / num2;
 };
 
+<<<<<<< HEAD
 /*
 const sum = function (arr) {
   let sum = arr.reduce((previousValue, currentValue) => {
@@ -85,3 +142,6 @@ const multiply = function (arr) {
   return sum;
 };
 */
+=======
+
+>>>>>>> acb630b (Calculator final version.)
